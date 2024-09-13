4001fdbd8289a7d6a930e48b54497294a3679a13
Michael Haberler <git@mah.priv.at>
2024-09-13 11:54:34 +0200

    camera, location workling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,135 @@
-import { Text, View } from "react-native";
+// import { Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
+
+// import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+// import { useState } from 'react';
+// import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+// export default function App() {
+//   const [facing, setFacing] = useState<CameraType>('back');
+//   const [permission, requestPermission] = useCameraPermissions();
+
+//   if (!permission) {
+//     // Camera permissions are still loading.
+//     return <View />;
+//   }
+
+//   if (!permission.granted) {
+//     // Camera permissions are not granted yet.
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.message}>We need your permission to show the camera</Text>
+//         <Button onPress={requestPermission} title="grant permission" />
+//       </View>
+//     );
+//   }
+
+//   function toggleCameraFacing() {
+//     setFacing(current => (current === 'back' ? 'front' : 'back'));
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <CameraView style={styles.camera} facing={facing}>
+//         <View style={styles.buttonContainer}>
+//           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+//             <Text style={styles.text}>Flip Camera</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </CameraView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   message: {
+//     textAlign: 'center',
+//     paddingBottom: 10,
+//   },
+//   camera: {
+//     flex: 1,
+//   },
+//   buttonContainer: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     backgroundColor: 'transparent',
+//     margin: 64,
+//   },
+//   button: {
+//     flex: 1,
+//     alignSelf: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: 'white',
+//   },
+// });
+
+// import { useState, useEffect } from 'react';
+// import { Platform, Text, View, StyleSheet } from 'react-native';
+
+// import * as Location from 'expo-location';
+
+// export default function App() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
+
+//   useEffect(() => {
+//     (async () => {
+      
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
+
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//     })();
+//   }, []);
+
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//   },
+//   paragraph: {
+//     fontSize: 18,
+//     textAlign: 'center',
+//   },
+// });
