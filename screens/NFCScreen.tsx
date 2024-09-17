876578cfbc79a3d24e78fe32b87ef224bf9ed659
Michael Haberler <git@mah.priv.at>
2024-09-17 13:10:25 +0200

    NFC works, but shaky

--- screens/NFCScreen.tsx
+++ screens/NFCScreen.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
-const NFCScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text> NFC Screen</Text>
-    </View>
-  );
-};
+NfcManager.start();
+
+export default function NFCScreen() {
+    async function performHaloInteraction() {
+        try {
+            await NfcManager.requestTechnology([NfcTech.IsoDep,
+            NfcTech.Ndef,
+            NfcTech.NfcA,
+            NfcTech.NfcB,
+            NfcTech.NfcF,
+            NfcTech.NfcV,
+            NfcTech.Iso15693IOS,
+            NfcTech.Iso15693IOS,
+            NfcTech.FelicaIOS,
+            NfcTech.MifareUltralight,
+            NfcTech.MifareIOS]);
+            const tag = await NfcManager.getTag();
+            const s = JSON.stringify(tag)
+            alert(`NFC tag has been scanned:  ${s}`);
+        } catch (ex) {
+            console.warn("Oops!", JSON.stringify(ex));
+        } finally {
+            // stop the nfc scanning
+            NfcManager.cancelTechnologyRequest();
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Click on the button and then scan the NFC tag. Results will appear in the console.</Text>
+            <TouchableOpacity style={{ padding: 100, backgroundColor: '#FF00FF' }} onPress={performHaloInteraction}>
+                <Text>Click here and tap the tag</Text>
+            </TouchableOpacity>
+            {/* <StatusBar style="auto" /> */}
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default NFCScreen;
